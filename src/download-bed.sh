e8f40dc8fa40567f3ad71136548b501479f1e108
Thomas McCarthy <twmccart@users.noreply.github.com>
2017-05-03 16:37:08 -0400

    incorporated all the scripts I've messed with this at this point

--- src/download-bed.sh
+++ src/download-bed.sh
@@ -3,15 +3,15 @@
 # script to download bam and bai files from SNP-Seek servers
 
 cult=$1
-mkdir -p ${cult}
-cd ${cult}
+#mkdir -p ${cult}
+#pushd ${cult}
 
 wget https://s3.amazonaws.com/3kricegenome/Nipponbare/${cult}.realigned.bam
-wget https://s3.amazonaws.com/3kricegenome/Nipponbare/${cult}.realigned.bam.bai
-wget https://s3.amazonaws.com/3kricegenome/Nipponbare/${cult}.realigned.bam.stats
+#wget https://s3.amazonaws.com/3kricegenome/Nipponbare/${cult}.realigned.bam.bai
+#wget https://s3.amazonaws.com/3kricegenome/Nipponbare/${cult}.realigned.bam.stats
 
 # this part requires samtools to be available 
-# module load samtools/1.3
+ module load samtools/1.3
 [ -f ${cult}.realigned.bam ] && ! [ -f ${cult}.realigned.bam.bai ] && samtools index ${cult}.realigned.bam
 
-cd ..
+#popd

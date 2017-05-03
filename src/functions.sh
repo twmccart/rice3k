a0b5af2a37d49a6a42d3b2b60b31d7806ff3e4b6
Thomas Wayne McCarthy <mccarthy.tw@gmail.com>
2017-05-03 16:37:08 -0400

    incorporated all the scripts I've messed with this at this point

--- src/functions.sh
+++ src/functions.sh
@@ -1,7 +1,52 @@
 #!/bin/bash
 
 
+# function that will generate the gvcf file for a given cultivar
 
+function call_variants {
+	which java
+	echo $GATK
+
+	CULT=$1
+
+	$GATK -T HaplotypeCaller \
+      		-R $REFERENCE \
+      		-I $MAPS/${CULT}.realigned.bam \
+      		-ERC GVCF \
+      		-o $CALLS/${CULT}.g.vcf \
+    		-nct 8
+}
+
+function genotype {
+	which java
+	echo $GATK
+
+	CULT=$1
+
+	$GATK -T GenotypeGVCFs \
+      		-R $REFERENCE \
+      		-V $CALLS/${CULT}.g.vcf \
+      		-o $CALLS/${CULT}.vcf
+}
+
+function full_genotype {
+	which java
+	echo $GATK
+
+	CULT=$1
+
+	$GATK -T GenotypeGVCFs \
+      		-R $REFERENCE \
+      		-V $CALLS/${CULT}.g.vcf \
+            -allSites \
+      		-o $CALLS/${CULT}.full.vcf \
+	    	-nt 24
+}
+
+
+# same as above, but only for a given loci rather than a full chromosome
+# loci can be in the gatk format chrname:begin-end
+# or it can be a file with a list of such formatted loci
 
 function call_variants_range {
 	which java
@@ -18,17 +63,4 @@ function call_variants_range {
       		-o $CALLS/${CULT}-${LOCI}.g.vcf
 }
 
-
-function call_variants {
-	which java
-	echo $GATK
-
-	CULT=$1
-
-	$GATK -T HaplotypeCaller \
-      		-R $REFERENCE \
-      		-I $MAPS/${CULT}.realigned.bam \
-      		-ERC GVCF \
-      		-o $CALLS/${CULT}-${LOCI}.g.vcf
-}
 

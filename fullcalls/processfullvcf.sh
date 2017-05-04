3911375c9142ec00afea8025b3a9ce7d986b714a
Thomas McCarthy <mccarthy.tw@gmail.com>
2017-05-04 11:58:04 -0400

    bcftools merge refuses to run on these full.vcf.gz files

---
+++ fullcalls/processfullvcf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+#find . -name "*.full.vcf" print0 | xargs -0 -I{} bgzip -c {} > {}.gz
+#find . -name "*.full.vcf.gz" print0 | xargs -0 tabix 
+
+#/home/twmccart/bcftools-1.4/bcftools merge -m both *.vcf.gz -O v -o 6test.merge.vcf
+vcf-merge *.vcf.gz > 6test.merge.vcf
+vcftools --remove-indels --non-ref-af-any 0.5 --max-missing 1 --recode -out 6test.merge.intersect
+#vcftools --vcf ${accession}.full.vcf --remove-indels --recode --stdout | bgzip -c > ${accession}.full.noindels.vcf.gz && \
+#tabix -p vcf ${accession}.full.noindels.vcf.gz

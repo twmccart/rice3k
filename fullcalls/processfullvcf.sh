330ff3033f2f88ff53146021fb38c1c105801c17
Thomas McCarthy <twmccart@users.noreply.github.com>
2017-05-08 11:13:14 -0400

    hrg

--- fullcalls/processfullvcf.sh
+++ fullcalls/processfullvcf.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 #find . -name "*.full.vcf" print0 | xargs -0 -I{} bgzip -c {} > {}.gz
 #find . -name "*.full.vcf.gz" print0 | xargs -0 tabix 
 
-#/home/twmccart/bcftools-1.4/bcftools merge -m both *.vcf.gz -O v -o 6test.merge.vcf
 vcf-merge *.vcf.gz > 6test.merge.vcf
 vcftools --remove-indels --non-ref-af-any 0.5 --max-missing 1 --recode -out 6test.merge.intersect
-#vcftools --vcf ${accession}.full.vcf --remove-indels --recode --stdout | bgzip -c > ${accession}.full.noindels.vcf.gz && \
-#tabix -p vcf ${accession}.full.noindels.vcf.gz
+
+#/home/twmccart/bcftools-1.4/bcftools merge -m both *full.vcf.gz -O v > 6test.bcftoolsmerge.vcf
+# -m2 is minimum of 2 alleles listed. 
+#/home/twmccart/bcftools-1.4/bcftools view --trim-alt-alleles --exclude-uncalled --exclude-types 'indels' -m 2 -O v 6test.bcftoolsmerge.vcf > 6test.bcftoolsview.intersect.vcf

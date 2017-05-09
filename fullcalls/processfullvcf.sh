d0cf568a88d0e4715a99832e1199dc65d4f73d49
Thomas McCarthy <twmccart@users.noreply.github.com>
2017-05-09 13:03:26 -0400

    processfullvcf.sh currently runs on a vcf file and removes indels (I think. Not 100% sure there were indels in the test inputs), heterozygous sites, and any uncalled sites. Uncommenting lines will make it into a script for first merging a list of vcf files and then doing all that cleaning.
    snp2seq.py was originally written by Murat. I cleaned it up according to the Google style sheet for Python to improve its readability, and I also changed a string append to a list append, because that is supposed to be much faster. The script now takes an input VCF file and produces a FASTA file of the sample sequences. It is suitable for processing a clean multi-sample VCF file into what is essentially a FASTA alignment.

--- fullcalls/processfullvcf.sh
+++ fullcalls/processfullvcf.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 #find . -name "*.full.vcf" print0 | xargs -0 -I{} bgzip -c {} > {}.gz
 #find . -name "*.full.vcf.gz" print0 | xargs -0 tabix 
 
-#/home/twmccart/bcftools-1.4/bcftools merge -m both *.vcf.gz -O v -o 6test.merge.vcf
-vcf-merge *.vcf.gz > 6test.merge.vcf
-bcftools view --exclude-uncalled --exclude-types 'indels' --min-ac 1 -O v -o 6test.merge.cleaned.vcf 6test.merge.vcf
-#vcftools --remove-indels --non-ref-af-any 0.5 --max-missing 1 --recode -out 6test.merge.intersect
-#vcftools --vcf ${accession}.full.vcf --remove-indels --recode --stdout | bgzip -c > ${accession}.full.noindels.vcf.gz && \
-#tabix -p vcf ${accession}.full.noindels.vcf.gz
+#vcf-merge *.vcf.gz | tee 6test.merge.vcf | bcftools view --exclude-uncalled --exclude-types 'indels' --min-ac 1 --genotype ^miss -O u | bcftools view --genotype ^het -O v | awk ' /^#/ {print} length($4) == 1 {print} ' > 6test.merge.cleaned.short.awk.vcf
+
+bcftools view --exclude-uncalled --exclude-types 'indels' --min-ac 1 --genotype ^miss -O u $1 | bcftools view --genotype ^het -O v | awk ' /^#/ {print} length($4) == 1 {print} ' > ${1}.cleaned

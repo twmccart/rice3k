9cab6f82cf0ec0aad58a57514d7042ed4e6e2dc7
Thomas McCarthy <twmccart@users.noreply.github.com>
2017-05-09 15:33:50 -0400

    added a flag to the clean-and-split function that will filter out any heterozygous sites

--- src/functions.sh
+++ src/functions.sh
@@ -73,8 +73,9 @@ function clean_vcf() {
 function clean_and_split_vcf() {
     cultivar=$1
 	for chromosome in chr{01,02,03,04,05,06,07,08,09,10,11,12}; do (
-		bcftools view --exclude-uncalled --exclude-types 'indels' -r ${chromosome} -O z -o ${cultivar}.${chromosome}.cleaned.vcf.gz ${cultivar}.full.vcf		tabix ${cultivar}.${chromosome}.cleaned.vcf.gz) &
+		bcftools view --exclude-uncalled --exclude-types 'indels' --genotype ^het -r ${chromosome} -O z -o ${cultivar}.${chromosome}.cleaned.vcf.gz ${cultivar}.full.vcf ; tabix ${cultivar}.${chromosome}.cleaned.vcf.gz) &
 	done
 	wait
+	echo "SPLIT IS FINISHED"
 }
 

204a9f1cf4716ca807512d96863b58ebff5228d5
Thomas Wayne McCarthy <mccarthy.tw@gmail.com>
2017-07-18 16:58:15 -0400

    bringing up to speed for collaboration with Samer

--- src/environment.sh
+++ src/environment.sh
@@ -6,12 +6,12 @@ export reads=${project_root}/reads
 export maps=${project_root}/maps
 export calls=${project_root}/calls
 export src=$project_root/src
-export logs=$project_root/log
+export log=$project_root/log
 export split=${project_root}/split
 export merges=${project_root}/merges
 export alignments=${project_root}/alignments
 
-mkdir -p $reads $maps $calls $logs $merges $split
+mkdir -p $reads $maps $calls $log $merges $split $alignments
 
 export picard="java -jar $src/picard.jar"
 export gatk="java -jar $src/GenomeAnalysisTK.jar"
